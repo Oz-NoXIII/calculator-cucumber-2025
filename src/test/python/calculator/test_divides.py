271ba8c5b38ae447d69aeb3065c97bb8d843b9be
Arsène Mujyabwami <181954@umons.ac.be>
2025-03-19 22:37:11 +0100

    test of all operation and notation

--- src/test/python/calculator/test_divides.py
+++ src/test/python/calculator/test_divides.py
@@ -1,9 +1,56 @@
 import unittest
 
+from src.main.python.calculator.divides import Divides
+from src.main.python.calculator.illegal_construction import IllegalConstruction
+from src.main.python.calculator.my_number import MyNumber
+from src.main.python.calculator.notation import Notation
+from src.main.python.calculator.times import Times
 
-class MyTestCase(unittest.TestCase):
-	def test_something(self):
-		self.assertEqual(True, False)  # add assertion here
+
+class TestDivides(unittest.TestCase):
+
+	value1 = 8
+	value2 = 6
+
+	def setUp(self):
+		params = [MyNumber(self.value1), MyNumber(self.value2)]
+		try:
+			self.op = Divides(params, Notation.INFIX)
+		except IllegalConstruction as e:
+			self.fail(e)
+
+	def test_constructor(self):
+		self.assertRaises(IllegalConstruction, lambda: Divides(None))
+		try:
+			self.assertIsNot(self.op, Times([]))
+		except IllegalConstruction as e:
+			self.fail(e)
+
+	def test_equals(self):
+		p = [MyNumber(self.value1), MyNumber(self.value2)]
+		try:
+			e = Divides(p, Notation.INFIX)
+			self.assertEqual(self.op, e)
+		except IllegalConstruction as e:
+			self.fail(e)
+
+	def test_None(self):
+		try:
+			result = (self.op is None)
+		except Exception as e:
+			self.fail(f"An exception was thrown: {e}")
+
+	def test_hash_code(self):
+		p = [MyNumber(self.value1), MyNumber(self.value2)]
+		try:
+			e = Divides(p, Notation.INFIX)
+			self.assertEqual(hash(self.op), hash(e))
+		except IllegalConstruction as e:
+			self.fail(e)
+
+	def test_none_param_list(self):
+		params = None
+		self.assertRaises(IllegalConstruction, lambda: Divides(params))
 
 
 if __name__ == '__main__':
--- src/test/python/calculator/test_operation.py
+++ src/test/python/calculator/test_operation.py
@@ -1,10 +1,37 @@
 import unittest
 
+from src.main.python.calculator.divides import Divides
+from src.main.python.calculator.minus import Minus
+from src.main.python.calculator.my_number import MyNumber
+from src.main.python.calculator.plus import Plus
+from src.main.python.visitor.counter import Counter
 
-class MyTestCase(unittest.TestCase):
-	def test_something(self):
-		self.assertEqual(True, False)  # add assertion here
 
+class TestOperation(unittest.TestCase):
+
+	def setUp(self):
+		params1 = [MyNumber(3), MyNumber(4), MyNumber(5)]
+		params2 = [MyNumber(5), MyNumber(4)]
+		params3 = [Plus(params1), Minus(params2), MyNumber(7)]
+		self.o = Divides(params3)
+		self.o2 = Divides(params3)
+		self.o.accept(Counter())
+		self.o2.accept(Counter())
+
+	def test_equals(self):
+		self.assertEqual(self.o, self.o2)
+
+	def test_hash_code(self):
+		self.assertEqual(hash(self.o), hash(self.o2))
+
+	def test_get_depth(self):
+		self.assertEqual(2, self.o.get_depth())
+
+	def test_get_ops(self):
+		self.assertEqual(3, self.o.get_ops())
+
+	def test_get_nbs(self):
+		self.assertEqual(6, self.o.get_nbs())
 
 if __name__ == '__main__':
 	unittest.main()
