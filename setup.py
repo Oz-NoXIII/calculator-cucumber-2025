66c35b7b5e0405ca1099eb82b3c8ad8d7364c5cf
Oz-NoXIII <Oz-NoXIII@users.noreply.github.com>
2025-04-05 11:09:08 +0000

    deploy: e99b9577fa3f4152abbb8618589a79eda2348e99

--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(
-    name="myproject",
-    version="0.1",
-    packages=find_packages(where="src/main/python"),
-    package_dir={"": "src/main/python"},
-    install_requires=open("requirements.txt").read().splitlines(),
-)
