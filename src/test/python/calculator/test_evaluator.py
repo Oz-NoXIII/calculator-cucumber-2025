bea79ac7c0b38f0b24627607feb42088f82546e1
Delplanque Nicolas <delplanque.n@outlook.com>
2025-04-17 11:55:32 +0200

    Format code

--- src/test/python/calculator/test_evaluator.py
+++ src/test/python/calculator/test_evaluator.py
@@ -1,6 +1,7 @@
 import unittest
 
 from parameterized import parameterized
+
 from src.main.python.calculator import calculator
 from src.main.python.calculator.divides import Divides
 from src.main.python.calculator.illegal_construction import IllegalConstruction
@@ -12,37 +13,50 @@ from src.main.python.calculator.times import Times
 
 class TestEvaluator(unittest.TestCase):
 
-	def setUp(self):
-		self.value1 = 8
-		self.value2 = 6
+    def setUp(self):
+        self.value1 = 8
+        self.value2 = 6
+
+    def test_evaluate_my_number(self):
+        self.assertEqual(self.value1, calculator.eval_expression(MyNumber(self.value1)))
+
+    @parameterized.expand(
+        [
+            ("*",),
+            ("+",),
+            ("/",),
+            ("-",),
+        ]
+    )
+    def test_evaluate_operations(self, symbol):
+        params = [MyNumber(self.value1), MyNumber(self.value2)]
+        try:
+            match symbol:
+                case "+":
+                    self.assertEqual(
+                        self.value1 + self.value2,
+                        calculator.eval_expression(Plus(params)),
+                    )
+                case "-":
+                    self.assertEqual(
+                        self.value1 - self.value2,
+                        calculator.eval_expression(Minus(params)),
+                    )
+                case "*":
+                    self.assertEqual(
+                        self.value1 * self.value2,
+                        calculator.eval_expression(Times(params)),
+                    )
+                case "/":
+                    self.assertEqual(
+                        self.value1 // self.value2,
+                        calculator.eval_expression(Divides(params)),
+                    )
+                case _:
+                    self.fail("Invalid symbol")
+        except IllegalConstruction as e:
+            self.fail(e)
 
 
-	def test_evaluate_my_number(self):
-		self.assertEqual(self.value1, calculator.eval_expression(MyNumber(self.value1)))
-
-	@parameterized.expand([
-		("*",),
-		("+",),
-		("/",),
-		("-",),
-	])
-	def test_evaluate_operations(self, symbol):
-		params = [MyNumber(self.value1), MyNumber(self.value2)]
-		try:
-			match symbol:
-				case "+":
-					self.assertEqual(self.value1 + self.value2, calculator.eval_expression(Plus(params)))
-				case "-":
-					self.assertEqual(self.value1 - self.value2, calculator.eval_expression(Minus(params)))
-				case "*":
-					self.assertEqual(self.value1 * self.value2, calculator.eval_expression(Times(params)))
-				case "/":
-					self.assertEqual(self.value1 // self.value2, calculator.eval_expression(Divides(params)))
-				case _:
-					self.fail("Invalid symbol")
-		except IllegalConstruction as e:
-			self.fail(e)
-
-
-if __name__ == '__main__':
-	unittest.main()
+if __name__ == "__main__":
+    unittest.main()
--- src/test/python/calculator/test_times.py
+++ src/test/python/calculator/test_times.py
@@ -9,43 +9,43 @@ from src.main.python.calculator.times import Times
 
 class TestTimes(unittest.TestCase):
 
-	value1 = 8
-	value2 = 6
+    value1 = 8
+    value2 = 6
 
-	def setUp(self):
-		params = [MyNumber(self.value1), MyNumber(self.value2)]
-		try:
-			self.op = Times(params)
-		except IllegalConstruction as e:
-			self.fail(e)
+    def setUp(self):
+        params = [MyNumber(self.value1), MyNumber(self.value2)]
+        try:
+            self.op = Times(params)
+        except IllegalConstruction as e:
+            self.fail(e)
 
-	def test_constructor(self):
-		self.assertRaises(IllegalConstruction, lambda: Times(None))
-		try:
-			self.assertIsNot(self.op, Plus([]))
-		except IllegalConstruction as e:
-			self.fail(e)
+    def test_constructor(self):
+        self.assertRaises(IllegalConstruction, lambda: Times(None))
+        try:
+            self.assertIsNot(self.op, Plus([]))
+        except IllegalConstruction as e:
+            self.fail(e)
 
-	def test_equals(self):
-		p = [MyNumber(self.value1), MyNumber(self.value2)]
-		try:
-			e = Times(p, Notation.INFIX)
-			self.assertEqual(self.op, e)
-		except IllegalConstruction as e:
-			self.fail(e)
+    def test_equals(self):
+        p = [MyNumber(self.value1), MyNumber(self.value2)]
+        try:
+            e = Times(p, Notation.INFIX)
+            self.assertEqual(self.op, e)
+        except IllegalConstruction as e:
+            self.fail(e)
 
-	def test_hash_code(self):
-		p = [MyNumber(self.value1), MyNumber(self.value2)]
-		try:
-			e = Times(p, Notation.INFIX)
-			self.assertEqual(hash(self.op), hash(e))
-		except IllegalConstruction as e:
-			self.fail(e)
+    def test_hash_code(self):
+        p = [MyNumber(self.value1), MyNumber(self.value2)]
+        try:
+            e = Times(p, Notation.INFIX)
+            self.assertEqual(hash(self.op), hash(e))
+        except IllegalConstruction as e:
+            self.fail(e)
 
-	def test_none_param_list(self):
-		params = None
-		self.assertRaises(IllegalConstruction, lambda: Times(params))
+    def test_none_param_list(self):
+        params = None
+        self.assertRaises(IllegalConstruction, lambda: Times(params))
 
 
-if __name__ == '__main__':
-	unittest.main()
+if __name__ == "__main__":
+    unittest.main()
