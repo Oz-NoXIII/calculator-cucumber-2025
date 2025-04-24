deabdc05140e77d166c5962517e16c6b749cca3f
Arsène Mujyabwami <181954@umons.ac.be>
2025-04-24 17:51:46 +0200

    Integer all notation parser and test

--- src/main/python/parsing/expression_parser.py
+++ src/main/python/parsing/expression_parser.py
@@ -2,45 +2,54 @@ from lark import Lark, Transformer, v_args
 
 from src.main.python.calculator.divides import Divides
 from src.main.python.calculator.integer_number import IntegerNumber
+from src.main.python.calculator.inverse import Inverse
 from src.main.python.calculator.minus import Minus
 from src.main.python.calculator.my_number import MyNumber
 from src.main.python.calculator.plus import Plus
+from src.main.python.calculator.power import Power
 from src.main.python.calculator.times import Times
 import os
 
 # Get the directory where the current script is located
 script_dir = os.path.dirname(os.path.abspath(__file__))
 # Construct the path to the grammar file
-grammar_path = os.path.join(script_dir, 'grammar', 'infix.lark')
+grammar_path = os.path.join(script_dir, 'grammar', 'notation.lark')
 # Load grammar from file or string
 with open(grammar_path, 'r') as f:
-    infix_grammar = f.read()
+    grammar = f.read()
 
-infix_parser = Lark(infix_grammar, parser='lalr', start='start')
+parser = Lark(grammar, parser='lalr', start='start')
 
 
-@v_args(inline=True)
 class ExprTransformer(Transformer):
+
+    @v_args(inline=True)
     def integer_number(self, token):
         return MyNumber(IntegerNumber(token))
 
-    def add(self, left, right):
-        return Plus([left, right])
+    def add(self, args):
+        return Plus(args)
 
-    def sub(self, left, right):
-        return Minus([left, right])
+    def sub(self, args):
+        return Minus(args)
 
-    def mul(self, left, right):
-        return Times([left, right])
+    def mul(self, args):
+        return Times(args)
 
-    def div(self, left, right):
-        return Divides([left, right])
+    def div(self, args):
+        return Divides(args)
 
+    @v_args(inline=True)
     def neg(self, token):
         return MyNumber(IntegerNumber(int(token.get_value()) * -1))
 
+    def pow(self, args):
+        return Power(args)
 
-def parse_expression(expr_str: str, notation='infix'):
-    if notation == 'infix':
-        tree = infix_parser.parse(expr_str)
-        return ExprTransformer().transform(tree)
+    @v_args(inline=True)
+    def inverse(self, token):
+        return Inverse([token])
+
+def parse_expression(expr_str: str):
+    tree = parser.parse(expr_str)
+    return ExprTransformer().transform(tree)
--- src/test/python/parsing/test_expression_parser.py
+++ src/test/python/parsing/test_expression_parser.py
@@ -1,17 +1,50 @@
 import unittest
 
 from src.main.python.calculator.divides import Divides
+from src.main.python.calculator.inverse import Inverse
 from src.main.python.calculator.minus import Minus
 from src.main.python.calculator.my_number import MyNumber
 from src.main.python.calculator.plus import Plus
+from src.main.python.calculator.power import Power
 from src.main.python.calculator.times import Times
 from src.main.python.parsing.expression_parser import parse_expression
 
 
 class TestExpressionParser(unittest.TestCase):
 
-    def test_parse_expression(self):
+    def test_parse_expression_infix(self):
+        # Infix notation
         expr_str = '1 + 2 * 3 - 4 / 5'
+        result = parse_expression(expr_str)
+        self.assertIsInstance(result, Minus)
+        self.assertEqual(len(result.get_args()), 2)
+        self.assertIsInstance(result.get_args()[0], Plus)
+        self.assertIsInstance(result.get_args()[1], Divides)
+        self.assertEqual(len(result.get_args()[0].get_args()), 2)
+        self.assertIsInstance(result.get_args()[0].get_args()[0], MyNumber)
+        self.assertIsInstance(result.get_args()[0].get_args()[1], Times)
+        self.assertEqual(len(result.get_args()[1].get_args()), 2)
+        self.assertIsInstance(result.get_args()[1].get_args()[0], MyNumber)
+        self.assertIsInstance(result.get_args()[1].get_args()[1], MyNumber)
+
+    def test_parse_expression_prefix(self):
+        # Prefix notation
+        expr_str = '-(+(1 *(2 3)) /(4 5))'
+        result = parse_expression(expr_str)
+        self.assertIsInstance(result, Minus)
+        self.assertEqual(len(result.get_args()), 2)
+        self.assertIsInstance(result.get_args()[0], Plus)
+        self.assertIsInstance(result.get_args()[1], Divides)
+        self.assertEqual(len(result.get_args()[0].get_args()), 2)
+        self.assertIsInstance(result.get_args()[0].get_args()[0], MyNumber)
+        self.assertIsInstance(result.get_args()[0].get_args()[1], Times)
+        self.assertEqual(len(result.get_args()[1].get_args()), 2)
+        self.assertIsInstance(result.get_args()[1].get_args()[0], MyNumber)
+        self.assertIsInstance(result.get_args()[1].get_args()[1], MyNumber)
+
+    def test_parse_expression_postfix(self):
+        # Prefix notation
+        expr_str = '((1 (2 3)*)+ (4 5)/)-'
         result = parse_expression(expr_str)
         self.assertIsInstance(result, Minus)
         self.assertEqual(len(result.get_args()), 2)
@@ -34,6 +67,21 @@ class TestExpressionParser(unittest.TestCase):
         self.assertEqual(len(result.get_args()[1].get_args()), 2)
         self.assertIsInstance(result.get_args()[1].get_args()[0], MyNumber)
         self.assertIsInstance(result.get_args()[1].get_args()[1], MyNumber)
+
+    def test_parse_expression_pow_inv(self):
+        expr_str = '1 ^ 2'
+        result = parse_expression(expr_str)
+        self.assertIsInstance(result, Power)
+        self.assertEqual(len(result.get_args()), 2)
+        self.assertIsInstance(result.get_args()[0], MyNumber)
+        self.assertIsInstance(result.get_args()[1], MyNumber)
+
+        expr_str = '1 ^ (inv(2))'
+        result = parse_expression(expr_str)
+        self.assertIsInstance(result, Power)
+        self.assertEqual(len(result.get_args()), 2)
+        self.assertIsInstance(result.get_args()[0], MyNumber)
+        self.assertIsInstance(result.get_args()[1], Inverse)
 
 
 if __name__ == '__main__':
