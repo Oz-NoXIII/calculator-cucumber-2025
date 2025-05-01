8b390c4dd70b22441e99c066dd2767caf2ff6ca9
Delplanque Nicolas <delplanque.n@outlook.com>
2025-05-01 19:14:22 +0200

    Add helper and unlock some features

--- src/web/src/App.jsx
+++ src/web/src/App.jsx
@@ -12,26 +12,30 @@ import {
   XIcon,
 } from 'lucide-react';
 import {cn} from "./lib/utils.js";
+import {Tabs,
+  TabsContent,
+  TabsList,
+  TabsTrigger,} from "./components/tabs.jsx";
 
-const base_buttons = [
+const calculator_buttons = [
   [
     { label: '2nd', value: '2nd', disabled: false, second: null },
-    { label: <PiIcon />, value: 'pi', disabled: true, second: { label: 'const', value: 'const' } },
-    { label: 'e', value: 'e', disabled: true, second: { label: 'i', value: 'i' } },
+    { label: <PiIcon />, value: 'pi', disabled: true, second: { label: 'const', value: 'const', disabled: true } },
+    { label: 'e', value: 'e', disabled: true, second: { label: 'j', value: 'j', disabled: false } },
     { label: '[::]', value: '[::]', disabled: true, second: null },
-    { label: 'x', value: 'x', disabled: true, second: { label: '=', value: '=' } },
-    { label: '(', value: '(', disabled: false, second: { label: '[', value: '[' } },
-    { label: ',', value: ',', disabled: true, second: null },
-    { label: ')', value: ')', disabled: false, second: { label: ']', value: ']' } },
+    { label: 'x', value: 'x', disabled: true, second: { label: '=', value: '=', disabled: true } },
+    { label: '(', value: '(', disabled: false, second: { label: '[', value: '[', disabled: true } },
+    { label: ',', value: ',', disabled: false, second: null },
+    { label: ')', value: ')', disabled: false, second: { label: ']', value: ']', disabled: true } },
     { label: <ArrowLeftRight />, value: 'swap', disabled: true, second: null },
     { label: <CornerDownLeft />, value: 'enter', disabled: true, second: null },
   ],
   [
-    { label: 'sin', value: 'sin', disabled: true, second: { label: 'sin⁻¹', value: 'asin' }},
-    { label: 'sinh', value: 'sinh', disabled: true, second: { label: 'sinh⁻¹', value: 'asinh' } },
-    { label: 'cot', value: 'cot', disabled: true, second: { label: 'cot⁻¹', value: 'acot' } },
+    { label: 'sin', value: 'sin', disabled: true, second: { label: 'sin⁻¹', value: 'asin', disabled: true }},
+    { label: 'sinh', value: 'sinh', disabled: true, second: { label: 'sinh⁻¹', value: 'asinh', disabled: true } },
+    { label: 'cot', value: 'cot', disabled: true, second: { label: 'cot⁻¹', value: 'acot', disabled: true } },
     { label: 'y√x', value: 'y_sqrt_x', disabled: true, second: null },
-    { label: 'xʸ', value: 'x_pow_y', disabled: true, second: null },
+    { label: 'xʸ', value: '^', disabled: false, second: null },
     { label: '7', value: '7', disabled: false, second: null },
     { label: '8', value: '8', disabled: false, second: null },
     { label: '9', value: '9', disabled: false, second: null },
@@ -39,11 +43,11 @@ const base_buttons = [
     { label: 'C', value: 'clear', disabled: false, second: null },
   ],
   [
-    { label: 'cos', value: 'cos', disabled: true, second: { label: 'cos⁻¹', value: 'acos' } },
-    { label: 'cosh', value: 'cosh', disabled: true, second: { label: 'cosh⁻¹', value: 'acosh' } },
-    { label: 'sec', value: 'sec', disabled: true, second: { label: 'sec⁻¹', value: 'asec' } },
+    { label: 'cos', value: 'cos', disabled: true, second: { label: 'cos⁻¹', value: 'acos', disabled: true } },
+    { label: 'cosh', value: 'cosh', disabled: true, second: { label: 'cosh⁻¹', value: 'acosh', disabled: true } },
+    { label: 'sec', value: 'sec', disabled: true, second: { label: 'sec⁻¹', value: 'asec', disabled: true } },
     { label: '³√x', value: 'cbrt', disabled: true, second: null },
-    { label: 'x³', value: 'x_cubed', disabled: true, second: null },
+    { label: 'x³', value: '^3', disabled: false, second: null },
     { label: '4', value: '4', disabled: false, second: null },
     { label: '5', value: '5', disabled: false, second: null },
     { label: '6', value: '6', disabled: false, second: null },
@@ -51,11 +55,11 @@ const base_buttons = [
     { label: <EqualIcon />, value: '=', disabled: false, second: null },
   ],
   [
-    { label: 'tan', value: 'tan', disabled: true, second: { label: 'tan⁻¹', value: 'atan' } },
-    { label: 'tanh', value: 'tanh', disabled: true, second: { label: 'tanh⁻¹', value: 'atanh' } },
-    { label: 'csc', value: 'csc', disabled: true, second: { label: 'csc⁻¹', value: 'acsc' } },
+    { label: 'tan', value: 'tan', disabled: true, second: { label: 'tan⁻¹', value: 'atan', disabled: true } },
+    { label: 'tanh', value: 'tanh', disabled: true, second: { label: 'tanh⁻¹', value: 'atanh', disabled: true } },
+    { label: 'csc', value: 'csc', disabled: true, second: { label: 'csc⁻¹', value: 'acsc', disabled: true } },
     { label: '√x', value: 'sqrt', disabled: true, second: null },
-    { label: 'x²', value: 'x_squared', disabled: true, second: null },
+    { label: 'x²', value: '^2', disabled: false, second: null },
     { label: '1', value: '1', disabled: false, second: null },
     { label: '2', value: '2', disabled: false, second: null },
     { label: '3', value: '3', disabled: false, second: null },
@@ -64,11 +68,11 @@ const base_buttons = [
   [
     { label: 'ncr', value: 'ncr', disabled: true, second: null },
     { label: 'npr', value: 'npr', disabled: true, second: null },
-    { label: <PercentIcon />, value: '%', disabled: false, second: null },
+    { label: <PercentIcon />, value: '%', disabled: true, second: null },
     { label: 'log', value: 'log', disabled: true, second: null },
-    { label: '10ˣ', value: '10_pow_x', disabled: true, second: null },
+    { label: '10ˣ', value: '10^', disabled: false, second: null },
     { label: '0', value: '0', disabled: false, second: null },
-    { label: <DiffIcon />, value: 'negate', disabled: false, second: null },
+    { label: <DiffIcon />, value: 'inv()', disabled: false, second: null },
     { label: '.', value: '.', disabled: false, second: null },
     { label: <PlusIcon />, value: '+', disabled: false, second: null },
     { label: "=dec", value: 'toDec', disabled: true, second: null },
@@ -86,8 +90,8 @@ const base_buttons = [
     { label: "=hex", value: 'toHex', disabled: true, second: null },
   ],
   [
-    { label: 'lsh', value: 'rsh', disabled: true, second: { label: 'rol', value: 'rol' } },
-    { label: 'rsh', value: 'rsh', disabled: true, second: { label: 'ror', value: 'ror' } },
+    { label: 'lsh', value: 'rsh', disabled: true, second: { label: 'rol', value: 'rol', disabled: true } },
+    { label: 'rsh', value: 'rsh', disabled: true, second: { label: 'ror', value: 'ror', disabled: true } },
     { label: '', value: '', disabled: true, second: null},
     { label: "lg2", value: 'lg2', disabled: true, second: null },
     { label: '2ˣ', value: '2_pow_x', disabled: true, second: null },
@@ -105,7 +109,9 @@ export default function App() {
   const inputRef = useRef(null);
 
   const handleClick = async (value) => {
-    inputRef.current.focus();
+    const input = inputRef.current;
+    input.focus();
+
     if (value === 'clear') {
       setExpression('');
     } else if (value === '=') {
@@ -129,25 +135,35 @@ export default function App() {
         setExpression('Erreur');
       }
     } else {
-      setExpression(expression + value);
+      const start = input.selectionStart;
+      const end = input.selectionEnd;
+
+      const newExpression =
+          expression.slice(0, start) + value + expression.slice(end);
+
+      setExpression(newExpression);
+
+      setTimeout(() => {
+        input.selectionStart = input.selectionEnd = start + value.length;
+      }, 0);
     }
   };
 
   return (
-    <div className="p-4 flex bg-black/95 justify-center items-center h-screen">
-      <div className="border max-w-3xl w-full rounded-lg bg-white flex flex-col p-4 shadow-xs">
+    <div className="px-6 py-30 gap-20 flex bg-black/95 justify-start items-start h-screen">
+      <div className="border max-w-3xl w-full rounded-lg bg-white h-full flex flex-col justify-start items-center p-4 shadow-xs">
         <h1 className="text-2xl font-semibold text-center mb-6">
           Calculator Cucumber
         </h1>
         <input
           ref={inputRef}
           type="text"
-          value={expression || '0'}
+          value={expression || ''}
+          onChange={(e)=> setExpression(e.target.value)}
           className="mb-4 p-4 w-full h-16 text-right text-2xl font-mono bg-gray-100 rounded focus-visible:outline-none"
-          readOnly
         />
         <div className="grid w-full grid-cols-10 grid-rows-7 gap-2">
-          {base_buttons.flat().map((btn, index) => {
+          {calculator_buttons.flat().map((btn, index) => {
             if (btn.value === '=') {
               return (
                 <button
@@ -176,7 +192,7 @@ export default function App() {
               if(second){
                 return(
                     <button
-                    disabled={btn.disabled}
+                    disabled={btn?.second ? btn.second.disabled: btn.disabled}
                     key={index}
                     onClick={() => {
                       if(btn?.second){
@@ -210,6 +226,153 @@ export default function App() {
             }
           })}
         </div>
+        <p className="mt-3 text-xs text-muted-foreground/40">By Arsène Mujyabwami, Ingrid Fondja Tchoumba, Nicolas Delplanque and Xavier Delabie </p>
+      </div>
+      <div className="flex flex-col gap-6 justify-start items-start h-full w-full bg-white rounded-md p-4">
+        <p className="text-lg font-medium">Helper</p>
+        <Tabs
+            defaultValue="tab-1"
+            orientation="vertical"
+            className="w-full h-full flex flex-row"
+        >
+          <TabsList className="flex-col justify-start rounded-none border-l h-fit bg-transparent p-0">
+            <TabsTrigger
+                value="tab-1"
+                className="data-[state=active]:after:bg-primary relative w-full justify-start rounded-none after:absolute after:inset-y-0 after:start-0 after:w-0.5 data-[state=active]:bg-transparent data-[state=active]:shadow-none"
+            >
+              Basics
+            </TabsTrigger>
+            <TabsTrigger
+                value="tab-2"
+                className="data-[state=active]:after:bg-primary relative w-full justify-start rounded-none after:absolute after:inset-y-0 after:start-0 after:w-0.5 data-[state=active]:bg-transparent data-[state=active]:shadow-none"
+            >
+              ?
+            </TabsTrigger>
+            <TabsTrigger
+                value="tab-3"
+                className="data-[state=active]:after:bg-primary relative w-full justify-start rounded-none after:absolute after:inset-y-0 after:start-0 after:w-0.5 data-[state=active]:bg-transparent data-[state=active]:shadow-none"
+            >
+              ?
+            </TabsTrigger>
+          </TabsList>
+          <div className="grow rounded-md border text-start">
+            <TabsContent value="tab-1" className="py-3 px-4 grid gap-3 grid-cols-2">
+              <div className="flex flex-col gap-3">
+                <p className="text-muted-foreground text-xs">
+                  Infix
+                </p>
+                <div className="grid pl-4 text-sm">
+                  <p>
+                    <span className="font-bold">Addition :</span> a + b
+                  </p>
+                  <p>
+                    <span className="font-bold">Subtraction :</span> a - b
+                  </p>
+                  <p>
+                    <span className="font-bold">Multiplication :</span> a * b
+                  </p>
+                  <p>
+                    <span className="font-bold">Division :</span> a / b
+                  </p>
+                  <p>
+                    <span className="font-bold">Exponentiation :</span> a ^ b
+                  </p>
+                  <p>
+                    <span className="font-bold">Negation :</span> (-a)
+                  </p>
+                  <p>
+                    <span className="font-bold">Inverse :</span> inv(a)
+                  </p>
+                </div>
+              </div>
+              <div className="flex flex-col gap-3">
+                <p className="text-muted-foreground text-xs">
+                  Prefix
+                </p>
+                <div className="grid pl-4 text-sm">
+                  <p>
+                    <span className="font-bold">Addition :</span> +(a, b) <span className="text-muted-foreground/60 font-medium text-xs">or</span> +(a b)
+                  </p>
+                  <p>
+                    <span className="font-bold">Subtraction :</span> -(a, b) <span className="text-muted-foreground/60 font-medium text-xs">or</span> -(a b)
+                  </p>
+                  <p>
+                    <span className="font-bold">Multiplication :</span> *(a, b) <span className="text-muted-foreground/60 font-medium text-xs">or</span> *(a b)
+                  </p>
+                  <p>
+                    <span className="font-bold">Division :</span> /(a, b) <span className="text-muted-foreground/60 font-medium text-xs">or</span> /(a b)
+                  </p>
+                  <p>
+                    <span className="font-bold">Exponentiation :</span> ^(a, b) <span className="text-muted-foreground/60 font-medium text-xs">or</span> ^(a b)
+                  </p>
+                  <p>
+                    <span className="font-bold">Negation :</span> (-(a))
+                  </p>
+                  <p>
+                    <span className="font-bold">Inverse :</span> inv(a)
+                  </p>
+                </div>
+              </div>
+              <div className="flex flex-col gap-3">
+                <p className="text-muted-foreground text-xs">
+                  Postfix
+                </p>
+                <div className="grid pl-4 text-sm">
+                  <p>
+                    <span className="font-bold">Addition :</span> (a, b)+ <span className="text-muted-foreground/60 font-medium text-xs">or</span> (a b)+
+                  </p>
+                  <p>
+                    <span className="font-bold">Subtraction :</span> (a, b)- <span className="text-muted-foreground/60 font-medium text-xs">or</span> (a b)-
+                  </p>
+                  <p>
+                    <span className="font-bold">Multiplication :</span> (a, b)* <span className="text-muted-foreground/60 font-medium text-xs">or</span> (a b)*
+                  </p>
+                  <p>
+                    <span className="font-bold">Division :</span> (a, b)/ <span className="text-muted-foreground/60 font-medium text-xs">or</span> (a b)/
+                  </p>
+                  <p>
+                    <span className="font-bold">Exponentiation :</span> (a, b)^ <span className="text-muted-foreground/60 font-medium text-xs">or</span> (a b)^
+                  </p>
+                  <p>
+                    <span className="font-bold">Negation :</span> (a-)-
+                  </p>
+                  <p>
+                    <span className="font-bold">Inverse :</span> inv(a)
+                  </p>
+                </div>
+              </div>
+              <div className="flex flex-col gap-3">
+                <p className="text-muted-foreground text-xs">
+                  Others
+                </p>
+                <div className="grid pl-4 text-sm">
+                  <p>
+                    <span className="font-bold">Multiple operands :</span> +(a, b, c)
+                  </p>
+                  <p>
+                    <span className="font-bold">Nested operations :</span> *(a, +(b, c))
+                  </p>
+                  <p>
+                    <span className="font-bold">Complex numbers :</span> a + bj
+                  </p>
+                  <p>
+                    <span className="font-bold">Parentheses :</span> (a + b) * c
+                  </p>
+                </div>
+              </div>
+            </TabsContent>
+            <TabsContent value="tab-2">
+              <p className="text-muted-foreground px-4 py-3 text-xs">
+                Content for Tab 2
+              </p>
+            </TabsContent>
+            <TabsContent value="tab-3">
+              <p className="text-muted-foreground px-4 py-3 text-xs">
+                Content for Tab 3
+              </p>
+            </TabsContent>
+          </div>
+        </Tabs>
       </div>
     </div>
   );
