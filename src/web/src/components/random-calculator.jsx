8524d07d52ed5b8d230882c7e1790f8b0818b6fa
Delplanque Nicolas <delplanque.n@outlook.com>
2025-05-16 00:14:54 +0200

    Last design fix

--- src/web/src/components/random-calculator.jsx
+++ src/web/src/components/random-calculator.jsx
@@ -1,6 +1,35 @@
 import { useRef, useState } from 'react';
 import { EqualIcon } from 'lucide-react';
 import { toast } from 'sonner';
+import { cn } from '../lib/utils';
+
+const calculator_buttons = [
+  [
+    { label: 'Random', value: 'rand()', disabled: false, second: null },
+    { label: 'j', value: 'j', disabled: false, second: null },
+    { label: 'C', value: 'clear', disabled: false, second: null },
+  ],
+  [
+    { label: '7', value: '7', disabled: false, second: null },
+    { label: '8', value: '8', disabled: false, second: null },
+    { label: '9', value: '9', disabled: false, second: null },
+  ],
+  [
+    { label: '4', value: '4', disabled: false, second: null },
+    { label: '5', value: '5', disabled: false, second: null },
+    { label: '6', value: '6', disabled: false, second: null },
+  ],
+  [
+    { label: '1', value: '1', disabled: false, second: null },
+    { label: '2', value: '2', disabled: false, second: null },
+    { label: '3', value: '3', disabled: false, second: null },
+  ],
+  [
+    { label: '0', value: '0', disabled: false, second: null },
+    { label: '.', value: '.', disabled: false, second: null },
+    { label: <EqualIcon />, value: '=', disabled: false, second: null },
+  ],
+];
 
 export default function RandomCalculator() {
   const [expression, setExpression] = useState('');
@@ -10,24 +39,40 @@ export default function RandomCalculator() {
     const input = inputRef.current;
     input.focus();
 
-    try {
-      const response = await fetch('http://localhost:8000/api/calculate/', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ expression: value }),
-      });
+    if (value === 'clear') {
+      setExpression('');
+    } else if (value === '=') {
+      try {
+        const response = await fetch('http://localhost:8000/api/calculate/', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ expression }),
+        });
 
-      if (!response.ok) {
-        toast.error('Error during the assessment. Check your expression !');
-      } else {
-        const data = await response.json();
-        setExpression(String(data.result));
+        if (!response.ok) {
+          toast.error('Error during the assessment. Check your expression !');
+        } else {
+          const data = await response.json();
+          setExpression(String(data.result));
+        }
+      } catch (error) {
+        console.error(error);
+        toast.error('Request error. Check if the backend is online !');
       }
-    } catch (error) {
-      console.error(error);
-      toast.error('Request error. Check if the backend is online !');
+    } else {
+      const start = input.selectionStart;
+      const end = input.selectionEnd;
+
+      const newExpression =
+        expression.slice(0, start) + value + expression.slice(end);
+
+      setExpression(newExpression);
+
+      setTimeout(() => {
+        input.selectionStart = input.selectionEnd = start + value.length;
+      }, 0);
     }
   };
 
@@ -36,30 +81,26 @@ export default function RandomCalculator() {
       <input
         ref={inputRef}
         type="text"
-        readOnly
         value={expression || ''}
+        onChange={(e) => setExpression(e.target.value)}
         className="mb-4 p-4 w-full h-16 text-right text-2xl font-mono bg-gray-100 rounded focus-visible:outline-none"
       />
-
       <div className="grid w-full grid-cols-3 grid-rows-7 gap-2">
-        <button
-          onClick={() => handleClick('random1')}
-          className="bg-white flex justify-center items-center border py-1.5 px-3 rounded hover:bg-gray-100 [&_svg]:size-4 disabled:text-gray-200 disabled:hover:bg-white"
-        >
-          Random Integer
-        </button>
-        <button
-          onClick={() => handleClick('random2')}
-          className="bg-white flex justify-center items-center border py-1.5 px-3 rounded hover:bg-gray-100 [&_svg]:size-4 disabled:text-gray-200 disabled:hover:bg-white"
-        >
-          Random Rational
-        </button>
-        <button
-          onClick={() => handleClick('random3')}
-          className="bg-white flex justify-center items-center border py-1.5 px-3 rounded hover:bg-gray-100 [&_svg]:size-4 disabled:text-gray-200 disabled:hover:bg-white"
-        >
-          Random Complex
-        </button>
+        {calculator_buttons.flat().map((btn, index) => {
+          return (
+            <button
+              disabled={btn.disabled}
+              key={index}
+              onClick={() => handleClick(btn.value)}
+              className={cn(
+                'bg-white relative flex justify-center items-center border py-3 rounded hover:bg-gray-100 [&_svg]:size-4 disabled:text-gray-200 disabled:hover:bg-white',
+                btn.value === '=' && 'bg-black text-white hover:bg-black/80',
+              )}
+            >
+              {btn.label}
+            </button>
+          );
+        })}
       </div>
     </>
   );
